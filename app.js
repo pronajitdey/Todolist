fb7a630996ec74a793dff430f7cbc80d101a5607
Pronajit Dey <deypronajit@gmail.com>
2022-09-08 22:02:58 +0530

    Update port, add start script

--- app.js
+++ app.js
@@ -120,6 +120,6 @@ app.post("/delete", function(req,res) {
   }
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server started successfully");
 });
